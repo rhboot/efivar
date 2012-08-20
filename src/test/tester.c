03d4901e989116b891c5215ff8cf6427c64d4612
Peter Jones <pjones@redhat.com>
2012-08-20 15:15:53 -0400

    Make test program actually find a failure that I need to address.

--- src/test/tester.c
+++ src/test/tester.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+
 #include <efivar.h>
 
-#if 0
-static efi_guid_t test_guid = EFI_GUID(0x84be9c3e,0x8a32,0x42c0,0x891c,0x4c,0xd3,0xb0,0x72,0xbe,0xcc);
-#endif
+#define TEST_GUID EFI_GUID(0x84be9c3e,0x8a32,0x42c0,0x891c,0x4c,0xd3,0xb0,0x72,0xbe,0xcc)
+
+static void
+clean_test_environment(void)
+{
+	efi_del_variable(TEST_GUID, "small");
+	efi_del_variable(TEST_GUID, "large");
+}
+
+#define report_error(str) ({fprintf(stderr, str); goto fail;})
 
 int main(void)
 {
@@ -12,13 +21,20 @@ int main(void)
 		return 0;
 	}
 
+	clean_test_environment();
 
-	int ret = -1;
-	
+	int ret = 1;
 
-#if 0
+	char smallvalue[] = "smallvalue";
+
+	int rc;
+	rc = efi_set_variable(TEST_GUID, "small",
+			      smallvalue, strlen(smallvalue)+1,
+			      EFI_VARIABLE_RUNTIME_ACCESS);
+	if (rc < 0)
+		report_error("small value test failed: %m\n");
+
 	ret = 0;
 fail:
-#endif
 	return ret;
 }
