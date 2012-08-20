63dd93acb5e613b0a80b0db8a2ebf19b5820165f
Peter Jones <pjones@redhat.com>
2012-08-20 15:05:35 -0400

    Update test case suite.

--- src/test/tester.c
+++ src/test/tester.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
+#include <efivar.h>
 
-// 84be9c3e-8a32-42c0-891c-4cd3b072becc
+#if 0
+static efi_guid_t test_guid = EFI_GUID(0x84be9c3e,0x8a32,0x42c0,0x891c,0x4c,0xd3,0xb0,0x72,0xbe,0xcc);
+#endif
 
 int main(void)
 {
+	if (!efi_variables_supported()) {
+		printf("UEFI variables not supported on this machine.\n");
+		return 0;
+	}
+
+
 	int ret = -1;
 	
 
+#if 0
 	ret = 0;
-goto fail:
+fail:
+#endif
 	return ret;
 }
