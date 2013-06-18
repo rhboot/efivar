f9ded96e86ffd2b3e1de2fe0c92955e93a58d763
Peter Jones <pjones@redhat.com>
2013-06-18 11:06:05 -0400

    Make "efivar --list" work.
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- src/efivar.c
+++ src/efivar.c
@@ -17,18 +17,95 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <popt.h>
 
 #include "efivar.h"
 
-int main(void)
+#define ACTION_LIST	0x1
+#define ACTION_PRINT	0x2
+
+#define GUID_FORMAT "%08x-%04x-%04x-%04x-%02x%02x%02x%02x%02x%02x"
+
+static void
+list_all_variables(void)
 {
 	efi_guid_t *guid;
 	char *name;
 	int rc;
+	while ((rc = efi_get_next_variable_name(&guid, &name)) > 0)
+		 printf(GUID_FORMAT "-%s\n",
+		 	guid->a, guid->b, guid->c, bswap_16(guid->d),
+			guid->e[0], guid->e[1], guid->e[2], guid->e[3],
+			guid->e[4], guid->e[5], name);
 
-	while ((rc = efi_get_next_variable_name(&guid, &name)) > 0) {
-		 printf("name: \"%s\"\n", name);
+	if (rc < 0) {
+		fprintf(stderr, "efivar: error listing variables: %m\n");
+		exit(1);
 	}
+}
+
+static int
+show_variable(char *name)
+{
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	int action = 0;
+	char *name = NULL;
+	poptContext optCon;
+	struct poptOption options[] = {
+		{NULL, '\0', POPT_ARG_INTL_DOMAIN, "efivar" },
+		{"list", 'l', POPT_ARG_VAL, &action,
+		 ACTION_LIST, "list current variables", NULL },
+		{"print", 'p', POPT_ARG_STRING, &name, 0,
+		 "variable name to print", NULL },
+		POPT_AUTOALIAS
+		POPT_AUTOHELP
+		POPT_TABLEEND
+	};
+	int rc;
+
+	optCon = poptGetContext("efivar", argc, (const char **)argv, options,0);
+	
+	rc = poptReadDefaultConfig(optCon, 0);
+	if (rc < 0) {
+		fprintf(stderr, "efivar: poprReadDefaultConfig failed: %s\n",
+			poptStrerror(rc));
+		exit(1);
+	}
+
+	while ((rc = poptGetNextOpt(optCon)) > 0)
+		;
+
+	if (rc < -1) {
+		fprintf(stderr, "efivar: Invalid argument: %s: %s\n",
+			poptBadOption(optCon, 0), poptStrerror(rc));
+		exit(1);
+	}
+
+	if (poptPeekArg(optCon)) {
+		fprintf(stderr, "efivar: Invalid Argument: \"%s\"\n",
+			poptPeekArg(optCon));
+		exit(1);
+	}
+
+	poptFreeContext(optCon);
+
+	if (name)
+		action |= ACTION_PRINT;
+
+	switch (action) {
+		case ACTION_LIST:
+			list_all_variables();
+			break;
+		case ACTION_PRINT:
+			show_variable(name);
+			break;
+	};
 
 	return 0;
 }
