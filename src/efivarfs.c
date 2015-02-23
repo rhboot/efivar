1275e406201b0ba80f7f05da54c39d1fce1d3503
Gaelle Nassiet <gaellex.nassiet@intel.com>
2015-02-23 18:05:45 +0100

    Fix pointer dereferencement in efivar
    Signed-off-by: Gaelle Nassiet <gaellex.nassiet@intel.com>

--- src/efivarfs.c
+++ src/efivarfs.c
@@ -79,6 +79,7 @@ efivarfs_get_variable_size(efi_guid_t guid, const char *name, size_t *size)
 		goto err;
 
 	struct stat statbuf;
+	bzero(&statbuf, sizeof(struct stat));
 	rc = stat(path, &statbuf);
 	if (rc < 0)
 		goto err;
@@ -135,6 +136,7 @@ efivarfs_get_variable(efi_guid_t guid, const char *name, uint8_t **data,
 	if (fd < 0)
 		goto err;
 
+	bzero(&statbuf, sizeof(struct stat));
 	rc = read(fd, &ret_attributes, sizeof (ret_attributes));
 	if (rc < 0)
 		goto err;
