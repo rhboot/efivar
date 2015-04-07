3ec5118e6718045e5270466aa291253b09752b01
Peter Jones <pjones@redhat.com>
2015-04-07 14:07:52 -0400

    Add a bunch of load option handling utilities to efiboot.
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- src/include/efivar/efiboot-loadopt.h
+++ src/include/efivar/efiboot-loadopt.h
@@ -22,9 +22,44 @@ typedef struct efi_load_option_s efi_load_option;
 
 extern ssize_t efi_make_load_option(uint8_t *buf, ssize_t size,
 				    uint32_t attributes, efidp dp,
-				    char *description,
+				    unsigned char *description,
 				    uint8_t *optional_data,
-				    size_t optional_data_size);
-extern efidp efi_load_option_path(efi_load_option *opt);
+				    size_t optional_data_size)
+	__attribute__((__nonnull__ (4,5)));
+
+extern efidp efi_load_option_path(efi_load_option *opt)
+	__attribute__((__nonnull__ (1)));
+extern const unsigned char const *efi_load_option_desc(efi_load_option *opt)
+	__attribute__((__visibility__ ("default")))
+	__attribute__((__nonnull__ (1)));
+extern uint32_t efi_load_option_attrs(efi_load_option *opt)
+	__attribute__((__nonnull__ (1)))
+	__attribute__((__visibility__ ("default")));
+extern void efi_load_option_attr_set(efi_load_option *opt, uint16_t attr)
+	__attribute__((__nonnull__ (1)))
+	__attribute__((__visibility__ ("default")));
+extern void efi_load_option_attr_clear(efi_load_option *opt, uint16_t attr)
+	__attribute__((__nonnull__ (1)))
+	__attribute__((__visibility__ ("default")));
+extern uint16_t efi_load_option_pathlen(efi_load_option *opt)
+	__attribute__((__nonnull__ (1)))
+	__attribute__((__visibility__ ("default")));
+extern int efi_load_option_optional_data(efi_load_option *opt, size_t opt_size,
+					 unsigned char **datap, size_t *len)
+	__attribute__((__nonnull__ (1,3)))
+	__attribute__((__visibility__ ("default")));
+
+extern ssize_t
+efi_load_option_args_from_file(uint8_t *buf, ssize_t size, char *filename)
+	__attribute__((__nonnull__ (3)))
+	__attribute__((__visibility__ ("default")));
+extern ssize_t
+efi_load_option_args_as_utf8(uint8_t *buf, ssize_t size, char *utf8)
+	__attribute__((__nonnull__ (3)))
+	__attribute__((__visibility__ ("default")));
+extern ssize_t
+efi_load_option_args_as_ucs2(uint16_t *buf, ssize_t size, char *utf8)
+	__attribute__((__nonnull__ (3)))
+	__attribute__((__visibility__ ("default")));
 
 #endif /* _EFIBOOT_LOADOPT_H */
--- src/loadopt.c
+++ src/loadopt.c
@@ -31,7 +31,7 @@ ssize_t
 __attribute__((__nonnull__ (4,5)))
 __attribute__((__visibility__ ("default")))
 efi_make_load_option(uint8_t *buf, ssize_t size, uint32_t attributes,
-		     efidp dp, char *description,
+		     efidp dp, unsigned char *description,
 		     uint8_t *optional_data, size_t optional_data_size)
 {
 	if (!dp || !description ||
@@ -72,13 +72,188 @@ efi_make_load_option(uint8_t *buf, ssize_t size, uint32_t attributes,
 	return sz;
 }
 
+uint32_t
+__attribute__((__nonnull__ (1)))
+__attribute__((__visibility__ ("default")))
+efi_load_option_attrs(efi_load_option *opt)
+{
+	return opt->attributes;
+}
+
+void
+__attribute__((__nonnull__ (1)))
+__attribute__((__visibility__ ("default")))
+efi_load_option_attr_set(efi_load_option *opt, uint16_t attr)
+{
+	opt->attributes |= attr;
+}
+
+void
+__attribute__((__nonnull__ (1)))
+__attribute__((__visibility__ ("default")))
+efi_load_option_attr_clear(efi_load_option *opt, uint16_t attr)
+{
+	opt->attributes &= ~attr;
+}
+
+uint16_t
+__attribute__((__nonnull__ (1)))
+__attribute__((__visibility__ ("default")))
+efi_load_option_pathlen(efi_load_option *opt)
+{
+	return opt->file_path_list_length;
+}
+
 efidp
+__attribute__((__nonnull__ (1)))
 __attribute__((__visibility__ ("default")))
 efi_load_option_path(efi_load_option *opt)
 {
 	char *p = (char *)opt;
+	if (!opt) {
+		errno = EINVAL;
+		return NULL;
+	}
 	efidp dp = (efidp)(p + sizeof (opt->attributes)
 		   + sizeof (opt->file_path_list_length)
-		   + ucs2len(opt->description, -1));
+		   + ucs2size(opt->description, -1));
 	return dp;
+}
+
+int
+__attribute__((__nonnull__ (1,3)))
+__attribute__((__visibility__ ("default")))
+efi_load_option_optional_data(efi_load_option *opt, size_t opt_size,
+			      unsigned char **datap, size_t *len)
+{
+	unsigned char *p = (unsigned char *)opt;
+	if (!opt || !datap) {
+		errno = EINVAL;
+		return -1;
+	}
+	*datap = (unsigned char *)(p + sizeof (opt->attributes)
+		   + sizeof (opt->file_path_list_length)
+		   + ucs2size(opt->description, -1)
+		   + opt->file_path_list_length);
+	if (len && opt_size > 0)
+		*len = (p+opt_size) - *datap;
+	return 0;
+}
+
+ssize_t
+__attribute__((__nonnull__ (3)))
+__attribute__((__visibility__ ("default")))
+efi_load_option_args_from_file(uint8_t *buf, ssize_t size, char *filename)
+{
+	int rc;
+	ssize_t ret = -1;
+	struct stat statbuf = { 0, };
+	int saved_errno;
+	FILE *f;
+
+	if (!filename || (!buf && size > 0)) {
+		errno = -EINVAL;
+		return -1;
+	}
+
+	f = fopen(filename, "r");
+	if (!f)
+		return -1;
+
+	rc = fstat(fileno(f), &statbuf);
+	if (rc < 0)
+		goto err;
+
+	if (size == 0)
+		return statbuf.st_size;
+
+	if (size < statbuf.st_size) {
+		errno = ENOSPC;
+		goto err;
+	}
+
+	ret = fread(buf, 1, statbuf.st_size, f);
+	if (ret < statbuf.st_size)
+		ret = -1;
+err:
+	saved_errno = errno;
+	if (f)
+		fclose(f);
+	errno = saved_errno;
+	return ret;
+}
+
+ssize_t
+__attribute__((__nonnull__ (3)))
+__attribute__((__visibility__ ("default")))
+efi_load_option_args_as_utf8(uint8_t *buf, ssize_t size, char *utf8)
+{
+	ssize_t req;
+	if (!utf8 || (!buf && size > 0)) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	/* we specifically want the storage size without NUL here,
+	 * not the size with NUL or the number of utf8 characters */
+	req = strlen(utf8);
+
+	if (size == 0)
+		return req;
+
+	if (size < req) {
+		errno = ENOSPC;
+		return -1;
+	}
+
+	memcpy(buf, utf8, req);
+	return req;
+}
+
+ssize_t
+__attribute__((__nonnull__ (3)))
+__attribute__((__visibility__ ("default")))
+efi_load_option_args_as_ucs2(uint16_t *buf, ssize_t size, uint8_t *utf8)
+{
+	ssize_t req;
+	if (!utf8 || (!buf && size > 0)) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	req = utf8len(utf8, -1) * sizeof(uint16_t);
+	if (size == 0)
+		return req;
+
+	if (size < req) {
+		errno = ENOSPC;
+		return -1;
+	}
+
+	return utf8_to_ucs2(buf, size, 0, utf8);
+}
+
+static unsigned char *last_desc;
+
+static void
+__attribute__((destructor))
+teardown(void)
+{
+	if (last_desc)
+		free(last_desc);
+	last_desc = NULL;
+}
+
+__attribute__((__nonnull__ (1)))
+__attribute__((__visibility__ ("default")))
+const unsigned char const *
+efi_load_option_desc(efi_load_option *opt)
+{
+	if (last_desc) {
+		free(last_desc);
+		last_desc = NULL;
+	}
+
+	last_desc = ucs2_to_utf8(opt->description, -1);
+	return last_desc;
 }
