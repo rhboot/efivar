rhboot/efivar

commit c0a2b3db1088af888d98c2223a648bc6f6274eea

Make makeguids generate guid-symbols.c instead of .S
Just makes it a little easier to debug.

Signed-off-by: Peter Jones <pjones@redhat.com>

src/makeguids.c
@@ -150,46 +150,37 @@ main(int argc, char *argv[])
 
 	fprintf(header, "#ifndef EFIVAR_GUIDS_H\n#define EFIVAR_GUIDS_H 1\n\n");
 
+	fprintf(symout, "#include <efivar.h>\n\n");
+
 	for (unsigned int i = 0; i < line-1; i++) {
-		if (!strcmp(outbuf[i].symbol, "efi_guid_zero"))
-			fprintf(symout, "\t.globl %s\n"
-					"\t.data\n"
-					"\t.balign 1\n"
-					"\t.type %s, %%object\n"
-					"\t.size %s, %s_end - %s\n",
-				"efi_guid_empty", "efi_guid_empty",
-				"efi_guid_empty", "efi_guid_empty",
-				"efi_guid_empty");
-		fprintf(symout, "\t.globl %s\n"
-				"\t.data\n"
-				"\t.balign 1\n"
-				"\t.type %s, %%object\n"
-				"\t.size %s, %s_end - %s\n"
-				"%s:\n",
-			outbuf[i].symbol,
-			outbuf[i].symbol,
-			outbuf[i].symbol,
-			outbuf[i].symbol,
-			outbuf[i].symbol,
-			outbuf[i].symbol);
-		if (!strcmp(outbuf[i].symbol, "efi_guid_zero"))
-			fprintf(symout, "efi_guid_empty:\n");
-
 		uint8_t *guid_data = (uint8_t *) &outbuf[i].guid;
-		for (unsigned int j = 0; j < sizeof (efi_guid_t); j++)
-			fprintf(symout,"\t.byte 0x%02x\n", guid_data[j]);
 
-		fprintf(symout, "%s_end:\n", outbuf[i].symbol);
 		if (!strcmp(outbuf[i].symbol, "efi_guid_zero")) {
-			fprintf(symout, "efi_guid_empty_end:\n");
+			fprintf(symout, "const efi_guid_t\n"
+				"__attribute__((__visibility__ (\"default\")))\n"
+				"efi_guid_empty = {0x1,0x0,0x0,0x0,{0x0,0x0,0x0,0x0,0x0,0x0}};\n\n");
+		}
+		if (!strcmp(outbuf[i].symbol, "efi_guid_zero")) {
 			fprintf(header, "extern const efi_guid_t efi_guid_empty __attribute__((__visibility__ (\"default\")));\n");
 		}
 
 		fprintf(header, "extern const efi_guid_t %s __attribute__((__visibility__ (\"default\")));\n", outbuf[i].symbol);
+
+		fprintf(symout, "const \n"
+			"__attribute__((__visibility__ (\"default\")))\n"
+			"efi_guid_t %s = {0x%02x%02x%02x%02x,0x%02x%02x,0x%02x%02x,0x%02x%02x,{0x%02x,0x%02x,0x%02x,0x%02x,0x%02x,0x%02x}};\n\n",
+			outbuf[i].symbol,
+			guid_data[3], guid_data[2],
+			  guid_data[1], guid_data[0],
+			guid_data[5], guid_data[4],
+			guid_data[7], guid_data[6],
+			guid_data[8], guid_data[9],
+			guid_data[10], guid_data[11],
+			  guid_data[12], guid_data[13],
+			  guid_data[14], guid_data[15]);
 	}
 
 	fprintf(header, "\n#endif /* EFIVAR_GUIDS_H */\n");
-	fprintf(symout, "#if defined(__linux__) && defined(__ELF__)\n.section .note.GNU-stack,\"\",%%progbits\n#endif");
 	fclose(header);
 	fclose(symout);
 

.gitignore
@@ -17,3 +17,4 @@ core.*
 src/efivar
 src/efivar-static
 src/makeguids
+src/guid-symbols.c

Make.defaults
@@ -37,6 +37,5 @@ ccldflags = $(cflags) $(CCLDFLAGS) $(LDFLAGS) \
 	$(if $(filter $(CCLD),clang),$(clang_ccldflags),) \
 	$(if $(filter $(CCLD),gcc),$(gcc_ccldflags),) \
 	$(call pkg-config-ccldflags)
-CPPFLAGS?=
 SOFLAGS=-shared
 LDLIBS=$(foreach lib,$(LIBS),-l$(lib)) $(call pkg-config-ldlibs)

src/Makefile
@@ -15,23 +15,23 @@ TARGETS=$(LIBTARGETS) $(STATICLIBTARGETS) $(BINTARGETS) $(PCTARGETS)
 LIBEFIBOOT_SOURCES = crc32.c creator.c disk.c gpt.c linux.c loadopt.c
 LIBEFIBOOT_OBJECTS = $(patsubst %.c,%.o,$(LIBEFIBOOT_SOURCES))
 LIBEFIVAR_SOURCES = dp.c dp-acpi.c dp-hw.c dp-media.c dp-message.c \
-	efivarfs.c export.c guid.c guids.S guid-symbols.S \
+	efivarfs.c export.c guid.c guids.S guid-symbols.c \
 	lib.c vars.c
 LIBEFIVAR_OBJECTS = $(patsubst %.S,%.o,$(patsubst %.c,%.o,$(LIBEFIVAR_SOURCES)))
 EFIVAR_SOURCES = efivar.c
-GENERATED_SOURCES = include/efivar/efivar-guids.h guid-symbols.S
+GENERATED_SOURCES = include/efivar/efivar-guids.h guid-symbols.c
 MAKEGUIDS_SOURCES = makeguids.c guid.c
 ALL_SOURCES=$(LIBEFIBOOT_SOURCES) $(LIBEFIVAR_SOURCES) $(MAKEGUIDS_SOURCES) \
 	$(wildcard include/efivar/*.h) $(GENERATED_SOURCES) $(EFIVAR_SOURCES)
 
 all : deps $(TARGETS)
 
-./guid-symbols.S : include/efivar/efivar-guids.h
+./guid-symbols.c : include/efivar/efivar-guids.h
 ./guids.bin : include/efivar/efivar-guids.h
 ./names.bin : include/efivar/efivar-guids.h
 include/efivar/efivar-guids.h : makeguids guids.txt
 	./makeguids guids.txt guids.bin names.bin \
-		guid-symbols.S include/efivar/efivar-guids.h
+		guid-symbols.c include/efivar/efivar-guids.h
 
 makeguids : CPPFLAGS+=-DEFIVAR_BUILD_ENVIRONMENT
 makeguids : LIBS=dl
@@ -68,7 +68,7 @@ deps :: $(ALL_SOURCES)
 
 clean : 
 	@rm -rfv *~ *.o *.a *.E *.so *.so.* *.pc *.bin .*.d \
-		makeguids guid-symbols.S include/efivar/efivar-guids.h \
+		makeguids guid-symbols.c include/efivar/efivar-guids.h \
 		$(BINTARGETS)
 	@# remove the deps files we used to create, as well.
 	@rm -rfv .*.P .*.h.P *.S.P include/efivar/.*.h.P
@@ -92,6 +92,6 @@ test : all
 .PHONY: test
 .SECONDARY : libefivar.so.$(VERSION) libefivar.so.$(MAJOR_VERSION)
 .SECONDARY : libefiboot.so.$(VERSION) libefiboot.so.$(MAJOR_VERSION)
-.SECONDARY : include/efivar/efivar-guids.h guid-symbols.S
+.SECONDARY : include/efivar/efivar-guids.h guid-symbols.c
 .INTERMEDIATE : guids.bin names.bin
 .PRECIOUS : guid-symbols.o makeguids

commit e538ad9a1d79621f4a28a8ab24e5f85dad06e12c

Make guids be in correct byte order both in text and ram...
This is all because the EFI spec defines:

typedef struct {
	UINT32 Data1;
	UINT16 Data2;
	UINT16 Data3;
	UINT8
	Data4[8];
} EFI_GUID;

	{0x8BE4DF61,0x93CA,0x11d2,\
		{0xAA,0x0D,0x00,0xE0,0x98,0x03,0x2B,0x8C}}

but prints:
8BE4DF61-93CA-11d2-AA0D-00E098032B8C

But we decided to be "smart" and did:

typedef struct {
	uint32_t        a;
	uint16_t        b;
	uint16_t        c;
	uint16_t        d;
	uint8_t         e[6];
} efi_guid_t;

Which means d is effectively in big endian while a, b, and c are little
endian.  This is awful.

We also have efi_guid_t in the public header, so things can take
sizeof() on it, which seems ill advised in hindsight.

Thankfully, we also have the EFI_GUID() macro, which was getting things
right, so if things memcmp()'d or efi_guid_cmp()'d on that, it'd get it
right, and efibootmgr is, in fact, using that (via EFI_GUID_GLOBAL)
instead.

So now, keep them all in memory as le32-le16-le16-be16-b-b-b-b-b-b at
all times.  The only programs that should break were ther ones that were
already broken, and they'll need a rebuild.

Also fix a bug introduced in c0a2b3db1088af888d98c2223a648bc6f6274eea
which introduced at "1" into some definitions of efi_guid_empty.

(yes, it really says "d2" not "D2")

Signed-off-by: Peter Jones <pjones@redhat.com>

src/guid.c
@@ -43,17 +43,26 @@ efi_guid_to_str(const efi_guid_t *guid, char **sp)
 
 	if (!sp) {
 		return snprintf(NULL, 0, GUID_FORMAT,
-				guid->a, guid->b, guid->c, bswap_16(guid->d),
+				le32_to_cpu(guid->a),
+				le16_to_cpu(guid->b),
+				le16_to_cpu(guid->c),
+				be16_to_cpu(guid->d),
 				guid->e[0], guid->e[1], guid->e[2], guid->e[3],
 				guid->e[4], guid->e[5]);
 	} else if (sp && *sp) {
 		return snprintf(*sp, GUID_LENGTH_WITH_NUL, GUID_FORMAT,
-				guid->a, guid->b, guid->c, bswap_16(guid->d),
+				le32_to_cpu(guid->a),
+				le16_to_cpu(guid->b),
+				le16_to_cpu(guid->c),
+				be16_to_cpu(guid->d),
 				guid->e[0], guid->e[1], guid->e[2], guid->e[3],
 				guid->e[4], guid->e[5]);
 	} else {
 		rc = asprintf(&ret, GUID_FORMAT,
-				guid->a, guid->b, guid->c, bswap_16(guid->d),
+				le32_to_cpu(guid->a),
+				le16_to_cpu(guid->b),
+				le16_to_cpu(guid->c),
+				be16_to_cpu(guid->d),
 				guid->e[0], guid->e[1], guid->e[2], guid->e[3],
 				guid->e[4], guid->e[5]);
 		if (rc >= 0)
@@ -172,17 +181,26 @@ efi_guid_to_id_guid(const efi_guid_t *guid, char **sp)
 	}
 	if (!sp) {
 		return snprintf(NULL, 0, "{"GUID_FORMAT"}",
-				guid->a, guid->b, guid->c, bswap_16(guid->d),
+				le32_to_cpu(guid->a),
+				le16_to_cpu(guid->b),
+				le16_to_cpu(guid->c),
+				be16_to_cpu(guid->d),
 				guid->e[0], guid->e[1], guid->e[2], guid->e[3],
 				guid->e[4], guid->e[5]);
 	} else if (sp && *sp) {
 		return snprintf(*sp, GUID_LENGTH_WITH_NUL+2, "{"GUID_FORMAT"}",
-				guid->a, guid->b, guid->c, bswap_16(guid->d),
+				le32_to_cpu(guid->a),
+				le16_to_cpu(guid->b),
+				le16_to_cpu(guid->c),
+				be16_to_cpu(guid->d),
 				guid->e[0], guid->e[1], guid->e[2], guid->e[3],
 				guid->e[4], guid->e[5]);
 	}
 	rc = asprintf(&ret, "{"GUID_FORMAT"}",
-			guid->a, guid->b, guid->c, bswap_16(guid->d),
+				le32_to_cpu(guid->a),
+				le16_to_cpu(guid->b),
+				le16_to_cpu(guid->c),
+				be16_to_cpu(guid->d),
 			guid->e[0], guid->e[1], guid->e[2], guid->e[3],
 			guid->e[4], guid->e[5]);
 	if (rc >= 0)

src/guid.h
@@ -24,6 +24,7 @@
 #include <limits.h>
 #include <stdlib.h>
 #include <string.h>
+#include "efivar_endian.h"
 
 #define GUID_FORMAT "%08x-%04x-%04x-%04x-%02x%02x%02x%02x%02x%02x"
 
@@ -105,6 +106,7 @@ text_to_guid(const char *text, efi_guid_t *guid)
 	if (check_segment_sanity(eightbytes, 8) < 0)
 		return -1;
 	guid->a = (uint32_t)strtoul(eightbytes, NULL, 16);
+	guid->a = cpu_to_le32(guid->a);
 
 	/* 84be9c3e-8a32-42c0-891c-4cd3b072becc
 	 *          ^ */
@@ -112,6 +114,7 @@ text_to_guid(const char *text, efi_guid_t *guid)
 	if (check_segment_sanity(fourbytes, 4) < 0)
 		return -1;
 	guid->b = (uint16_t)strtoul(fourbytes, NULL, 16);
+	guid->b = cpu_to_le16(guid->b);
 
 	/* 84be9c3e-8a32-42c0-891c-4cd3b072becc
 	 *               ^ */
@@ -119,13 +122,15 @@ text_to_guid(const char *text, efi_guid_t *guid)
 	if (check_segment_sanity(fourbytes, 4) < 0)
 		return -1;
 	guid->c = (uint16_t)strtoul(fourbytes, NULL, 16);
+	guid->c = cpu_to_le16(guid->c);
 
 	/* 84be9c3e-8a32-42c0-891c-4cd3b072becc
 	 *                    ^ */
 	strncpy(fourbytes, text+19, 4);
 	if (check_segment_sanity(fourbytes, 4) < 0)
 		return -1;
-	guid->d = bswap_16((uint16_t)strtoul(fourbytes, NULL, 16));
+	guid->d = (uint16_t)strtoul(fourbytes, NULL, 16);
+	guid->d = cpu_to_be16(guid->d);
 
 	/* 84be9c3e-8a32-42c0-891c-4cd3b072becc
 	 *                         ^ */

src/include/efivar/efivar.h
@@ -18,6 +18,7 @@
 #ifndef EFIVAR_H
 #define EFIVAR_H 1
 
+#include <endian.h>
 #include <errno.h>
 #include <stdarg.h>
 #include <stdint.h>
@@ -39,8 +40,13 @@ typedef struct {
 #include <efivar-guids.h>
 #endif
 
+#if BYTE_ORDER == LITTLE_ENDIAN
 #define EFI_GUID(a,b,c,d,e0,e1,e2,e3,e4,e5) \
 ((efi_guid_t) {(a), (b), (c), bswap_16(d), { (e0), (e1), (e2), (e3), (e4), (e5) }})
+#else
+#define EFI_GUID(a,b,c,d,e0,e1,e2,e3,e4,e5) \
+((efi_guid_t) {(a), (b), (c), (d), { (e0), (e1), (e2), (e3), (e4), (e5) }})
+#endif
 
 #define EFI_GLOBAL_GUID EFI_GUID(0x8be4df61,0x93ca,0x11d2,0xaa0d,0x00,0xe0,0x98,0x03,0x2b,0x8c)
 

src/makeguids.c
@@ -147,7 +147,21 @@ main(int argc, char *argv[])
 
 	fprintf(header, "#ifndef EFIVAR_GUIDS_H\n#define EFIVAR_GUIDS_H 1\n\n");
 
-	fprintf(symout, "#include <efivar.h>\n\n");
+	fprintf(symout, "#include <efivar.h>\n");
+	fprintf(symout, "#include <endian.h>\n");
+	fprintf(symout, """\n\
+#if BYTE_ORDER == BIG_ENDIAN\n\
+#define cpu_to_be32(n) (n)\n\
+#define cpu_to_be16(n) (n)\n\
+#define cpu_to_le32(n) (__bswap_constant_32(n))\n\
+#define cpu_to_le16(n) (__bswap_constant_16(n))\n\
+#else\n\
+#define cpu_to_le32(n) (n)\n\
+#define cpu_to_le16(n) (n)\n\
+#define cpu_to_be32(n) (__bswap_constant_32(n))\n\
+#define cpu_to_be16(n) (__bswap_constant_16(n))\n\
+#endif\n\
+""");
 
 	for (unsigned int i = 0; i < line-1; i++) {
 		uint8_t *guid_data = (uint8_t *) &outbuf[i].guid;
@@ -158,7 +172,7 @@ main(int argc, char *argv[])
 		if (!strcmp(outbuf[i].symbol, "efi_guid_zero")) {
 			fprintf(symout, "const efi_guid_t\n"
 				"__attribute__((__visibility__ (\"default\")))\n"
-				"efi_guid_empty = {0x1,0x0,0x0,0x0,{0x0,0x0,0x0,0x0,0x0,0x0}};\n\n");
+				"efi_guid_empty = {0x0,0x0,0x0,0x0,{0x0,0x0,0x0,0x0,0x0,0x0}};\n\n");
 		}
 		if (!strcmp(outbuf[i].symbol, "efi_guid_zero")) {
 			fprintf(header, "extern const efi_guid_t efi_guid_empty __attribute__((__visibility__ (\"default\")));\n");
@@ -168,7 +182,7 @@ main(int argc, char *argv[])
 
 		fprintf(symout, "const \n"
 			"__attribute__((__visibility__ (\"default\")))\n"
-			"efi_guid_t %s = {0x%02x%02x%02x%02x,0x%02x%02x,0x%02x%02x,0x%02x%02x,{0x%02x,0x%02x,0x%02x,0x%02x,0x%02x,0x%02x}};\n\n",
+			"efi_guid_t %s = {cpu_to_le32(0x%02x%02x%02x%02x),cpu_to_le16(0x%02x%02x),cpu_to_le16(0x%02x%02x),cpu_to_be16(0x%02x%02x),{0x%02x,0x%02x,0x%02x,0x%02x,0x%02x,0x%02x}};\n\n",
 			outbuf[i].symbol,
 			guid_data[3], guid_data[2],
 			  guid_data[1], guid_data[0],