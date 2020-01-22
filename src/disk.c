rhboot/efivar

commit 038afd7a792df5d882d4ca29cc6f9723f3c3fc0f

Always refer to MBR and GPT fixed values as 'magic' not 'signature'
Signed-off-by: Peter Jones <pjones@redhat.com>

src/disk.c
@@ -52,10 +52,11 @@ is_mbr_valid(legacy_mbr *mbr)
 	int ret;
 	if (!mbr)
 		return 0;
-	ret = (mbr->signature == MSDOS_MBR_SIGNATURE);
+	ret = (mbr->magic == MSDOS_MBR_MAGIC);
 	if (!ret) {
 		errno = ENOTTY;
-		efi_error("mbr signature is not MSDOS_MBR_SIGNATURE");
+		efi_error("mbr magic is 0x%04hx not MSDOS_MBR_MAGIC (0x%04hx)",
+			  mbr->magic, MSDOS_MBR_MAGIC);
 	}
 	return ret;
 }

src/gpt.c
@@ -72,24 +72,24 @@ efi_crc32(const void *buf, unsigned long len)
  *
  * Description: Returns 1 if PMBR is valid, 0 otherwise.
  * Validity depends on two things:
- *  1) MSDOS signature is in the last two bytes of the MBR
+ *  1) MSDOS magic is in the last two bytes of the MBR
  *  2) One partition of type 0xEE is found
  */
 static int
 is_pmbr_valid(legacy_mbr *mbr)
 {
-	int i, found = 0, signature = 0;
+	int i, found = 0, magic = 0;
 	if (!mbr)
 		return 0;
-	signature = (le16_to_cpu(mbr->signature) == MSDOS_MBR_SIGNATURE);
-	for (i = 0; signature && i < 4; i++) {
+	magic = (le16_to_cpu(mbr->magic) == MSDOS_MBR_MAGIC);
+	for (i = 0; magic && i < 4; i++) {
 		if (mbr->partition[i].os_type ==
 		    EFI_PMBR_OSTYPE_EFI_GPT) {
 			found = 1;
 			break;
 		}
 	}
-	return (signature && found);
+	return (magic && found);
 }
 
 /**
@@ -389,11 +389,11 @@ is_gpt_valid(int fd, uint64_t lba,
 	if (!(*gpt = alloc_read_gpt_header(fd, lba)))
 		return 0;
 
-	/* Check the GUID Partition Table signature */
-	if (le64_to_cpu((*gpt)->signature) != GPT_HEADER_SIGNATURE) {
-		efi_error("GUID Partition Table Header signature is wrong: %"PRIx64" != %"PRIx64,
-			  (uint64_t)le64_to_cpu((*gpt)->signature),
-			  GPT_HEADER_SIGNATURE);
+	/* Check the GUID Partition Table magic */
+	if (le64_to_cpu((*gpt)->magic) != GPT_HEADER_MAGIC) {
+		efi_error("GUID Partition Table Header magic is wrong: %"PRIx64" != %"PRIx64,
+			  (uint64_t)le64_to_cpu((*gpt)->magic),
+			  GPT_HEADER_MAGIC);
 		free(*gpt);
 		*gpt = NULL;
 		return rc;
@@ -673,7 +673,7 @@ find_valid_gpt(int fd, gpt_header ** gpt, gpt_entry ** ptes,
 
 	/* Would fail due to bad PMBR, but force GPT anyhow */
 	if ((good_pgpt || good_agpt) && !good_pmbr && ignore_pmbr_err) {
-		efi_error("  Warning: Disk has a valid GPT signature but invalid PMBR.\n"
+		efi_error("  Warning: Disk has a valid GPT magic but invalid PMBR.\n"
 			  "  Use GNU Parted to correct disk.\n"
 			  "  gpt option taken, disk treated as GPT.");
 	}

src/gpt.h
@@ -29,10 +29,10 @@
 
 #define EFI_PMBR_OSTYPE_EFI 0xEF
 #define EFI_PMBR_OSTYPE_EFI_GPT 0xEE
-#define MSDOS_MBR_SIGNATURE 0xaa55
+#define MSDOS_MBR_MAGIC 0xaa55
 #define GPT_BLOCK_SIZE 512
 
-#define GPT_HEADER_SIGNATURE ((uint64_t)(0x5452415020494645ULL))
+#define GPT_HEADER_MAGIC ((uint64_t)(0x5452415020494645ULL))
 #define GPT_HEADER_REVISION_V1_02 0x00010200
 #define GPT_HEADER_REVISION_V1_00 0x00010000
 #define GPT_HEADER_REVISION_V0_99 0x00009900
@@ -61,7 +61,7 @@
                  0x23, 0x8f, 0x2a, 0x3d, 0xf9, 0x28)
 
 typedef struct _gpt_header {
-	uint64_t signature;
+	uint64_t magic;
 	uint32_t revision;
 	uint32_t header_size;
 	uint32_t header_crc32;
@@ -133,7 +133,7 @@ typedef struct _legacy_mbr {
 	uint32_t unique_mbr_signature;
 	uint16_t unknown;
 	partition_record partition[4];
-	uint16_t signature;
+	uint16_t magic;
 } PACKED legacy_mbr;
 
 #define EFI_GPT_PRIMARY_PARTITION_TABLE_LBA 1

commit da2356e07dbfda31dae2a8fd49cca8ed5125365e

Fix an argument mismatch error
MSDOS_MBR_MAGIC is an int, no need to specify the 'h' (unsigned short)
modifier. This avoids the error (built with clang):

external/efivar/src/disk.c:59:18: error: format specifies type 'unsigned short' but the argument has type 'int' [-Werror,-Wformat]
                          mbr->magic, MSDOS_MBR_MAGIC);
                                      ^~~~~~~~~~~~~~~
external/efivar/src/include/efivar/efivar.h:244:66: note: expanded from macro 'efi_error'
        efi_error_real__(errno, __FILE__, __func__, __LINE__, (fmt), ## args)
                                                                        ^~~~
external/efivar/src/include/efivar/efivar.h:241:56: note: expanded from macro 'efi_error_real__'
        efi_error_set(file, function, line, errval, (fmt), ## args)
                                                              ^~~~
external/efivar/src/gpt.h:32:25: note: expanded from macro 'MSDOS_MBR_MAGIC'
 #define MSDOS_MBR_MAGIC 0xaa55
                        ^~~~~~
1 error generated.

Fixes: 038afd7a ("Always refer to MBR and GPT fixed values as 'magic' not 'signature'")

Signed-off-by: Chih-Wei Huang <cwhuang@linux.org.tw>

src/disk.c
@@ -55,7 +55,7 @@ is_mbr_valid(legacy_mbr *mbr)
 	ret = (mbr->magic == MSDOS_MBR_MAGIC);
 	if (!ret) {
 		errno = ENOTTY;
-		efi_error("mbr magic is 0x%04hx not MSDOS_MBR_MAGIC (0x%04hx)",
+		efi_error("mbr magic is 0x%04hx not MSDOS_MBR_MAGIC (0x%04x)",
 			  mbr->magic, MSDOS_MBR_MAGIC);
 	}
 	return ret;