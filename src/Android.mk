rhboot/efivar

commit 3ba12ff9ba5ebcb55c1e3a8c720e265fa05888b2

Move our infrastructure makefiles out of the topdir.
No sense in leaving all this cluttering up what's usually my working
directory...

Signed-off-by: Peter Jones <pjones@redhat.com>

src/Android.mk
@@ -46,7 +46,7 @@ LIBEFIVAR_SOURCES := \
 	lib.c \
 	vars.c
 
-include $(LOCAL_PATH)/../Make.version
+include $(LOCAL_PATH)/../version.mk
 
 LOCAL_SRC_FILES := $(LIBEFIBOOT_SOURCES) $(LIBEFIVAR_SOURCES)
 LOCAL_CFLAGS := -D_FILE_OFFSET_BITS=64 -D_GNU_SOURCE -std=gnu11 -DLIBEFIVAR_VERSION=$(VERSION)

Makefile
@@ -1,15 +1,15 @@
-TOPDIR = $(shell echo $$PWD)
+export TOPDIR = $(realpath $(dir $(firstword $(MAKEFILE_LIST))))
 
-include $(TOPDIR)/Make.deprecated
-include $(TOPDIR)/Make.version
-include $(TOPDIR)/Make.rules
-include $(TOPDIR)/Make.defaults
-include $(TOPDIR)/Make.coverity
-include $(TOPDIR)/Make.scan-build
+include $(TOPDIR)/src/include/deprecated.mk
+include $(TOPDIR)/src/include/version.mk
+include $(TOPDIR)/src/include/rules.mk
+include $(TOPDIR)/src/include/defaults.mk
+include $(TOPDIR)/src/include/coverity.mk
+include $(TOPDIR)/src/include/scan-build.mk
 
 SUBDIRS := src docs
 
-all : | efivar.spec Make.version
+all : | efivar.spec src/include/version.mk
 all :
 	@set -e ; for x in $(SUBDIRS) ; do \
 		$(MAKE) -C $$x $@ ; \
@@ -43,7 +43,7 @@ a :
 
 GITTAG = $(shell bash -c "echo $$(($(VERSION) + 1))")
 
-efivar.spec : | Makefile Make.version
+efivar.spec : | Makefile src/include/version.mk
 
 clean :
 	@set -e ; for x in $(SUBDIRS) ; do \
@@ -66,8 +66,8 @@ test-archive: abicheck efivar.spec
 	@echo "The archive is in efivar-$(GITTAG).tar.bz2"
 
 bumpver :
-	@echo VERSION=$(GITTAG) > Make.version
-	@git add Make.version
+	@echo VERSION=$(GITTAG) > src/include/version.mk
+	@git add src/include/version.mk
 	git commit -m "Bump version to $(GITTAG)" -s
 
 tag:

docs/Makefile
@@ -1,10 +1,9 @@
 SRCDIR = $(realpath .)
-TOPDIR = $(realpath ..)
 
-include $(TOPDIR)/Make.deprecated
-include $(TOPDIR)/Make.version
-include $(TOPDIR)/Make.rules
-include $(TOPDIR)/Make.defaults
+include $(TOPDIR)/src/include/deprecated.mk
+include $(TOPDIR)/src/include/version.mk
+include $(TOPDIR)/src/include/rules.mk
+include $(TOPDIR)/src/include/defaults.mk
 
 MAN1TARGETS = efivar.1
 MAN3TARGETS = efi_append_variable.3 \
@@ -51,4 +50,4 @@ install :
 
 .PHONY: all clean install
 
-include $(TOPDIR)/Make.rules
+include $(TOPDIR)/src/include/rules.mk

src/Makefile
@@ -1,10 +1,9 @@
 SRCDIR = $(realpath .)
-TOPDIR = $(realpath ..)
 
-include $(TOPDIR)/Make.deprecated
-include $(TOPDIR)/Make.version
-include $(TOPDIR)/Make.rules
-include $(TOPDIR)/Make.defaults
+include $(TOPDIR)/src/include/deprecated.mk
+include $(TOPDIR)/src/include/version.mk
+include $(TOPDIR)/src/include/rules.mk
+include $(TOPDIR)/src/include/defaults.mk
 
 LIBTARGETS=libefivar.so libefiboot.so
 STATICLIBTARGETS=libefivar.a libefiboot.a
@@ -84,7 +83,7 @@ libefiboot.so : LIBS=efivar
 libefiboot.so : MAP=libefiboot.map
 
 deps : $(ALL_SOURCES)
-	@$(MAKE) -f $(SRCDIR)/Make.deps deps SOURCES="$(ALL_SOURCES)"
+	@$(MAKE) -f $(SRCDIR)/include/deps.mk deps SOURCES="$(ALL_SOURCES)"
 
 clean : 
 	@rm -rfv *~ *.o *.a *.E *.so *.so.* *.pc *.bin .*.d *.map \

src/include/defaults.mk
@@ -34,7 +34,7 @@ CPPFLAGS += -DLIBEFIVAR_VERSION=$(VERSION)
 clang_cflags = -D_GNU_SOURCE -std=gnu11 -Wno-address-of-packed-member \
 	       -funsigned-char -Wall -Wno-nonnull-compare \
 	       -Werror -Wno-error=cpp
-gcc_cflags = -specs=$(TOPDIR)/gcc.specs
+gcc_cflags = -specs=$(TOPDIR)/src/include/gcc.specs
 cflags	= $(CFLAGS) -I${TOPDIR}/src/include/ \
 	$(if $(findstring clang,$(CC)),$(clang_cflags),) \
 	$(if $(findstring ccc-analyzer,$(CC)),$(clang_cflags),) \

src/include/deps.mk
@@ -1,11 +1,10 @@
 SRCDIR = $(realpath .)
-TOPDIR = $(realpath ..)
 
 all : deps
 
-include $(TOPDIR)/Make.version
-include $(TOPDIR)/Make.rules
-include $(TOPDIR)/Make.defaults
+include $(TOPDIR)/src/include/version.mk
+include $(TOPDIR)/src/include/rules.mk
+include $(TOPDIR)/src/include/defaults.mk
 
 .%.d : %.c
 	$(CC) $(cflags) $(CPPFLAGS) -MM -MG -MF $@ $^

commit 835e6f85d767c70797e67e0ab949f2fa6bf75936

Android: fix incorrect include path
Fixes: 3ba12ff9 ("Move our infrastructure makefiles out of the topdir")

Signed-off-by: Chih-Wei Huang <cwhuang@linux.org.tw>

src/Android.mk
@@ -46,7 +46,7 @@ LIBEFIVAR_SOURCES := \
 	lib.c \
 	vars.c
 
-include $(LOCAL_PATH)/../version.mk
+include $(LOCAL_PATH)/include/version.mk
 
 LOCAL_SRC_FILES := $(LIBEFIBOOT_SOURCES) $(LIBEFIVAR_SOURCES)
 LOCAL_CFLAGS := -D_FILE_OFFSET_BITS=64 -D_GNU_SOURCE -std=gnu11 -DLIBEFIVAR_VERSION=$(VERSION)