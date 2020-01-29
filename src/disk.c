rhboot/efivar

commit 3cf3b3d5cf3f6ff7ddb315676e338a0632aa6dc8

Fix an argument mismatch error
MSDOS_MBR_MAGIC is an int, no need to specify the 'h' (unsigned short)
modifier. This avoids the error (built with clang):

external/efivar/src/disk.c:59:18: error: format specifies type 'unsigned short' but the argument has type 'int' [-Werror,-Wformat]
                          mbr->magic, MSDOS_MBR_MAGIC);
                                      ^~~~~~~~~~~~~~~
external/efivar/src/include/efivar/efivar.h:244:66: note: expanded from macro 'efi_error'
        efi_error_real__(errno, __FILE__, __func__, __LINE__, (fmt), ## args)
                                                                        ^~~~
external/efivar/src/include/efivar/efivar.h:241:56: note: expanded from macro 'efi_error_real__'
        efi_error_set(file, function, line, errval, (fmt), ## args)
                                                              ^~~~
external/efivar/src/gpt.h:32:25: note: expanded from macro 'MSDOS_MBR_MAGIC'
 #define MSDOS_MBR_MAGIC 0xaa55
                        ^~~~~~
1 error generated.

Fixes: 038afd7a ("Always refer to MBR and GPT fixed values as 'magic' not 'signature'")

Signed-off-by: Chih-Wei Huang <cwhuang@linux.org.tw>

src/disk.c
@@ -55,7 +55,7 @@ is_mbr_valid(legacy_mbr *mbr)
 	ret = (mbr->magic == MSDOS_MBR_MAGIC);
 	if (!ret) {
 		errno = ENOTTY;
-		efi_error("mbr magic is 0x%04hx not MSDOS_MBR_MAGIC (0x%04hx)",
+		efi_error("mbr magic is 0x%04hx not MSDOS_MBR_MAGIC (0x%04x)",
 			  mbr->magic, MSDOS_MBR_MAGIC);
 	}
 	return ret;

commit db5e7f4244150b108970093fdc97072934e2bf2a

Fix the MSDOS_MBR_MAGIC type, not the format string.
The value of MSDOS_MBR_MAGIC is 16 bits; make it evaluate that way,
instead of promoting it to int for efi_error() formatting.

Fixes: 3cf3b3d5cf3f6ff7ddb315676e338a0632aa6dc8

Signed-off-by: Peter Jones <pjones@redhat.com>

src/disk.c
@@ -55,7 +55,7 @@ is_mbr_valid(legacy_mbr *mbr)
 	ret = (mbr->magic == MSDOS_MBR_MAGIC);
 	if (!ret) {
 		errno = ENOTTY;
-		efi_error("mbr magic is 0x%04hx not MSDOS_MBR_MAGIC (0x%04x)",
+		efi_error("mbr magic is 0x%04hx not MSDOS_MBR_MAGIC (0x%04hx)",
 			  mbr->magic, MSDOS_MBR_MAGIC);
 	}
 	return ret;

src/gpt.h
@@ -27,9 +27,9 @@
 
 #include <inttypes.h>
 
-#define EFI_PMBR_OSTYPE_EFI 0xEF
-#define EFI_PMBR_OSTYPE_EFI_GPT 0xEE
-#define MSDOS_MBR_MAGIC 0xaa55
+#define EFI_PMBR_OSTYPE_EFI ((uint8_t)0xEF)
+#define EFI_PMBR_OSTYPE_EFI_GPT ((uint8_t)0xEE)
+#define MSDOS_MBR_MAGIC ((uint16_t)0xaa55)
 #define GPT_BLOCK_SIZE 512
 
 #define GPT_HEADER_MAGIC ((uint64_t)(0x5452415020494645ULL))