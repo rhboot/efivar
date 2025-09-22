3564e7d6cd3f2b16eeb12bc68b62728771863699
Peter Jones <pjones@redhat.com>
2025-09-22 16:14:19 -0400

    sbchooser: add some empty files and Make stuff.
    This gets us a binary that builds for sbchooser, but doesn't add any
    functionality yet.
    
    Signed-off-by: Peter Jones <pjones@redhat.com>

---
+++ src/sbchooser-x509.c
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-v3-or-later
+/*
+ * sbchooser-x509.c - x509 handling
+ * Copyright Peter Jones <pjones@redhat.com>
+ */
+
+#include "sbchooser.h" // IWYU pragma: keep
+
+// vim:fenc=utf-8:tw=75:noet
---
+++ src/sbchooser-x509.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-v3-or-later
+/*
+ * sbchooser-x509.h - x509 handling
+ * Copyright Peter Jones <pjones@redhat.com>
+ */
+
+#pragma once
+
+#include "sbchooser.h" // IWYU pragma: keep
+
+// vim:fenc=utf-8:tw=75:noet
