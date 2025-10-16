486013411f5cbd681fbcc546a2f64ff10f5924f3
Bernhard Froehlich <decke@bluelife.at>
2025-10-16 11:21:07 +0000

    Fix tm_to_efi_time() on FreeBD which has no global timezone variable in time.h
    Signed-off-by: Bernhard Froehlich <decke@bluelife.at>

--- src/time.c
+++ src/time.c
@@ -52,7 +52,17 @@ tm_to_efi_time(const struct tm * const s, efi_time_t *d, bool tzadj)
 
 	if (tzadj) {
 		tzset();
+
+#if defined(__FreeBSD__)
+		time_t t = time(NULL);
+		struct tm lt = {0};
+
+		localtime_r(&t, &lt);
+
+		d->timezone = lt.tm_gmtoff / 60;
+#else
 		d->timezone = timezone / 60;
+#endif
 	}
 
 	return 0;
