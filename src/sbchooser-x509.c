8bce87d03a8c0cd26318b5d15f10f58f97815878
Peter Jones <pjones@redhat.com>
2026-02-13 15:31:11 -0500

    sbchooser: parse secdb entries
    This parses the db and dbx entries, extracts the relevant authenticode
    hashes and x509 details, and provides the comparitor for determining
    which certificate has the highest security strength based on its digest
    and encryption algorithms.
    
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- src/sbchooser-x509.c
+++ src/sbchooser-x509.c
@@ -6,4 +6,188 @@
 
 #include "sbchooser.h" // IWYU pragma: keep
 
+void
+fmt_time(const ASN1_TIME *asn1, char buf[1024])
+{
+	struct tm tm;
+
+	memset(buf, 0, 1024);
+	if (!asn1) {
+		strcpy(buf, "(null)");
+		return;
+	}
+	ASN1_TIME_to_tm(asn1, &tm);
+	asctime_r(&tm, buf);
+
+	buf[1023] = '\0';
+	for (size_t i = 0; i < 1024; i++) {
+		if (buf[i] == '\r' || buf[i] == 'n' || !isprint(buf[i]))
+			buf[i] = '\0';
+	}
+}
+
+int
+time_cmp(const ASN1_TIME *t0, const ASN1_TIME *t1)
+{
+	int rc = 0;
+	char str0[1024];
+	char str1[1024];
+
+	memset(str0, 0, 1024);
+	memset(str1, 0, 1024);
+	fmt_time(t0, str0);
+	fmt_time(t1, str1);
+
+	if (t0 && t1)
+		rc = ASN1_TIME_compare(t0, t1);
+	if (t0 && !t1)
+		rc = -1;
+	if (t1 && !t0)
+		rc = 1;
+	debug("comparing \"%s\" to \"%s\": %d", str0, str1, rc);
+	return rc;
+}
+
+void
+free_cert(cert_data_t *cert)
+{
+	if (!cert)
+		return;
+
+	if (cert->free_x509 && cert->x509) {
+		X509_free(cert->x509);
+		cert->x509 = NULL;
+	}
+	free(cert);
+}
+
+int
+elaborate_x509_info(cert_data_t *cert)
+{
+	int rc;
+	const char *mdsn = NULL;
+	const char *pksn = NULL;
+
+	cert->subject = X509_get_subject_name(cert->x509);
+	if (!cert->subject) {
+		warnx("couldn't get cert subject");
+		goto err;
+	}
+
+	cert->issuer = X509_get_issuer_name(cert->x509);
+	if (!cert->issuer) {
+		warnx("couldn't get cert issuer");
+		goto err;
+	}
+
+	cert->serial = X509_get_serialNumber(cert->x509);
+	if (cert->serial == NULL) {
+		warnx("couldn't get cert serial");
+		goto err;
+	}
+
+	cert->not_before = X509_get0_notBefore(cert->x509);
+	if (cert->not_before == NULL) {
+		warnx("couldn't get not_before");
+		goto err;
+	}
+
+	cert->not_after = X509_get0_notAfter(cert->x509);
+	if (cert->not_after == NULL) {
+		warnx("couldn't get not_after");
+		goto err;
+	}
+
+	rc = X509_get_signature_info(cert->x509, &cert->md_nid, &cert->pk_nid,
+				     NULL, NULL);
+	if (rc != 1) {
+		warnx("couldn't get signature info");
+		goto err;
+	}
+
+	debug("md_nid:%d pk_nid:%d secbits:%d",
+	      cert->md_nid, cert->pk_nid, cert->md_secbits);
+	mdsn = OBJ_nid2sn(cert->md_nid);
+	pksn = OBJ_nid2ln(cert->pk_nid);
+
+	/*
+	 * We do not believe in OpenSSL's concept of md secbits; we believe
+	 * in NIST SP 800-57 Part 1 Rev. 5 page 69's.
+	 */
+	if (!strcmp(mdsn, "SHA512")) {
+		cert->md_secbits = 256;
+	} else if (!strcmp(mdsn, "SHA384")) {
+		cert->md_secbits = 192;
+	} else if (!strcmp(mdsn, "SHA256")) {
+		cert->md_secbits = 128;
+	} else if (!strcmp(mdsn, "SHA224")) {
+		cert->md_secbits = 112;
+	} else if (!strcmp(mdsn, "SHA1")) {
+		cert->md_secbits = 80;
+	}
+
+	debug("md:%s md_secbits:%d", mdsn, cert->md_secbits);
+
+	/*
+	 * XXX:PJFIX PQC isn't implemented here yet
+	 */
+	while (true) {
+		if (!strcmp(pksn, "rsaEncryption")) {
+			X509_PUBKEY *xpk;
+			ASN1_OBJECT *ppkalg = NULL;
+			const unsigned char *pk = NULL;
+			int pklen = 0;
+			X509_ALGOR *pa = NULL;
+			int rc = 0;
+
+			xpk = X509_get_X509_PUBKEY(cert->x509);
+			if (!xpk) {
+				debug("This certificate has no public key.  Not scoring.");
+				break;
+			}
+
+			rc = X509_PUBKEY_get0_param(&ppkalg, &pk, &pklen, &pa, xpk);
+			if (!rc) {
+				debug("This public key has no algorithm parameters.  Not scoring.");
+				break;
+			}
+			/*
+			 * PK here is the ASN1 encoded modulus and
+			 * exponent, so pklen (in bytes) includes a few
+			 * extra bytes here and there, but these values are
+			 * far enough apart it shouldn't matter...
+			 */
+			pklen *= 8;
+			if (pklen >= 15360) {
+				cert->pk_secbits = 256;
+			} else if (pklen >= 7690) {
+				cert->pk_secbits = 192;
+			} else if (pklen >= 3072) {
+				cert->pk_secbits = 128;
+			} else if (pklen >= 2048) {
+				cert->pk_secbits = 112;
+			} else if (pklen >= 1024) {
+				cert->pk_secbits = 80;
+			}
+		}
+		break;
+	}
+	debug("pk:%s pk_secbits:%d", pksn, cert->pk_secbits);
+
+	return 0;
+err:
+	X509_free(cert->x509);
+	cert->x509 = NULL;
+	return -1;
+}
+
+int
+cert_sec_cmp(cert_data_t *cert0, cert_data_t *cert1)
+{
+	int cert0_secbits = cert0->md_secbits < cert0->pk_secbits ? cert0->md_secbits : cert0->pk_secbits;
+	int cert1_secbits = cert1->md_secbits < cert1->pk_secbits ? cert1->md_secbits : cert1->pk_secbits;
+
+	return cert1_secbits - cert0_secbits;
+}
+
 // vim:fenc=utf-8:tw=75:noet
